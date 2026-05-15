cce8fe359090d6eb477d8aa1028952c50c904a4f
EmanuelPedrini <mistydorydory@gmail.com>
2026-05-15 10:43:41 -0300

    Mud

---
+++ REMAKE/CLASSES/ENTITY.py
@@ -0,0 +1,1 @@
+#Quando chegar em casa vou passar um molde de entity de outro projeto pra esse, deve funcionar perfeitamente
---
+++ REMAKE/PUBLIC/Public_Standards.py
@@ -0,0 +1,6 @@
+#ESSE ARQUIVO DEVE CONTER TODOS OS IMPORTS NÃO INTERNOS
+import random, time
+from enum import Enum, auto
+from dataclasses import dataclass
+import json
+pu
